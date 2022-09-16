344c2bf2e2f0709d7316aba0e0bdc8a9bfbb427f
Pavel Yosifovich <zodiacon@live.com>
2022-09-16 08:31:10 -0400

    fixed: switched between ALPC and thread events

--- EventTracing/KernelEvents.cpp
+++ EventTracing/KernelEvents.cpp
@@ -16,7 +16,7 @@ const std::vector<KernelEventCategory> KernelEvents {
 		}
 	},
 
-	{ L"ALPC", KernelEventTypes::Thread, &ThreadGuid,
+	{ L"ALPC", KernelEventTypes::ALPC, &ThreadGuid,
 		{
 			{ L"Send Message", 0x21 },
 			{ L"Receive Message", 0x22 },
@@ -30,7 +30,7 @@ const std::vector<KernelEventCategory> KernelEvents {
 		}, true
 	},
 
-	{ L"Thread", KernelEventTypes::ALPC, &ALPCGuid,
+	{ L"Thread", KernelEventTypes::Thread, &ALPCGuid,
 		{
 			{ L"Start", 1 },
 			{ L"End", 2 },
